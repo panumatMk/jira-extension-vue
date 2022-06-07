ceb9abd06ed107917fa4e546b3326f9d880e29b9
panumatMk <panumat.mk@gmail.com>
2022-06-07 12:02:32 +0700

    add google-extension

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
-import {createApp} from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 // import router from './router/index.ts.temp'
-import PrimeVue from 'primevue/config';
-import InputNumber from 'primevue/inputnumber';
-import Calendar from 'primevue/calendar';
+import PrimeVue from "primevue/config";
+import InputNumber from "primevue/inputnumber";
+import Calendar from "primevue/calendar";
 
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
-import 'primevue/resources/themes/bootstrap4-dark-blue/theme.css';
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primevue/resources/themes/bootstrap4-dark-blue/theme.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
 // app.use(router)
-app.use(PrimeVue)
+app.use(PrimeVue);
 
-app.component('InputNumber', InputNumber);
-app.component('Calendar', Calendar);
+app.component("InputNumber", InputNumber);
+app.component("Calendar", Calendar);
 
-app.mount('#app')
+app.mount("#app");
